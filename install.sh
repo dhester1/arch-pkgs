b0f263dd58bcb4be1636718e8e88b73d3c319a5f
dhester1 <45162198+dhester1@users.noreply.github.com>
2022-04-01 00:01:14 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ archroot () {
 	
 	chfn -f "${fullname}" "${username}"
 	mount "$part_boot" /boot/efi
-	grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+	grub-install --target=x86_64-efi --bootloader-id=Arch --efi-directory=/boot/efi
 	grub-mkconfig -o /boot/grub/grub.cfg
 }
 
