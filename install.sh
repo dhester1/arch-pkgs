4be5a0d8341b83b0b301055927ba885b82224368
dhester1 <45162198+dhester1@users.noreply.github.com>
2022-04-05 01:06:49 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ archroot (){
 	 nerd-fonts-complete ttf-ms-fonts \
 	 authy bottles firefox-extension-bitwarden ocs-url onlyoffice-bin \
 	 pamac-aur protontricks soundux visual-studio-code-bin winetricks; \
-	 sh -c \"$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" \"\" --unattended; \
+	 (curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) | sh; \
 	 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions; \
 	 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting; \
 	 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k; \
